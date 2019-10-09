604db49949c3a6745713aec09fc184d399e1e362
FPCoder <standardly2@gmail.com>
2019-10-08 23:41:20 -0700

    upload current state

---
+++ calculator/Numpad.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculator
+{
+    /// <summary>
+    /// Interaction logic for Numpad.xaml
+    /// </summary>
+    public partial class Numpad : UserControl, INotifyPropertyChanged
+    {
+        public Numpad()
+        {
+            InitializeComponent();
+        }
+
+        private string _value;
+
+        public string Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Value = (string)(sender as Button).Content;
+        }
+    }
+}
